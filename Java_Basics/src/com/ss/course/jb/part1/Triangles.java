22a48127367ccbf18e1c7bddeeed7458c46bfc47
Eric Colvin <ecolvin@mail.csuchico.edu>
2021-04-20 22:20:45 -0700

    Part 1 Code

---
+++ Java_Basics/src/com/ss/course/jb/part1/Triangles.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package com.ss.course.jb.part1;
+
+import java.lang.Math;
+
+/**
+ * Print 4 different types of triangles
+ * @author ecolv
+ *
+ */
+public class Triangles {
+
+	/**
+	 * Calls each of the 4 triangle methods with 4 rows 
+	 * @param args - command line arguments (unused)
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangles tri = new Triangles();
+		
+		tri.increasing(4);
+		System.out.println();
+		System.out.println();
+		
+		tri.decreasing(4);
+		System.out.println();
+		System.out.println();
+		
+		tri.upArrow(4);
+		System.out.println();
+		System.out.println();
+		
+		tri.downArrow(4);
+	}
+	
+	/**
+	 * Print a right triangle with an increasing # of stars in each row	
+	 * @param rows - # of rows of stars to print
+	 */
+	public void increasing(Integer rows) {
+		for(int i = 1; i <= rows; i++)
+		{
+			for(int j = 0; j < i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+		System.out.println("----------");
+	}
+	
+	/**
+	 * Print a right triangle with a decreasing # of stars in each row	
+	 * @param rows - # of rows of stars to print
+	 */
+	public void decreasing(Integer rows) {
+		System.out.println("----------");
+		for(int i = rows; i > 0; i--)
+		{
+			for(int j = 0; j < i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
+	/**
+	 * Print an isosceles triangle with an increasing # of stars in each row	
+	 * @param rows - # of rows of stars to print
+	 */
+	public void upArrow(Integer rows) {
+		for(int i = 1; i <= rows; i++) {
+			int numSpaces = (10-(2*(i-1)))/2;
+			for(int s = 0; s < numSpaces; s++) { 
+				System.out.print(' ');
+			}
+			for(int j = 0; j < (2*(i-1))+1; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+		System.out.println("-----------");
+	}
+
+	/**
+	 * Print an isosceles triangle with a decreasing # of stars in each row	
+	 * @param rows - # of rows of stars to print
+	 */
+	public void downArrow(Integer rows) {
+		System.out.println("-----------");
+		for(int i = rows; i > 0; i--) {
+			int numSpaces = (10-(2*(i-1)))/2;
+			for(int s = 0; s < numSpaces; s++) {
+				System.out.print(' ');
+			}
+			for(int j = 0; j < (2*(i-1))+1; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
+
+}
